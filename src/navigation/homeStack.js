1d2f54a977f4514e4cc86731af59f04d996c86ce
Samuel Olwe <sam.olwe@gmail.com>
2021-02-28 21:01:45 +0300

    Created stack for home screen

---
+++ src/navigation/homeStack.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/homeScreen';
+import HeaderIcon from '../components/headers/headerIcon';
+import styled from 'styled-components';
+
+const Stack = createStackNavigator();
+
+export default function HomeStack({route, navigation}) {
+  //   const navigation = useNavigation();
+  return (
+    <Stack.Navigator initialRouteName="HomeScreen" headerMode="screen">
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          headerTitle: (props) => (
+            <LogoTitle
+              title="Map"
+              {...props}
+              onPress={() => {
+                navigation.toggleDrawer();
+              }}
+            />
+          ),
+          headerStyle: {
+            backgroundColor: '#1e96dc',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerLeft: () => (
+            <HeaderIcon
+              size={25}
+              source={require('../assets/dash.png')}
+              onPress={() => {
+                let nav = navigation.dangerouslyGetParent();
+                nav.toggleDrawer();
+              }}
+            />
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function LogoTitle(props) {
+  return (
+    <Container>
+      <Title>{props.title}</Title>
+      <HeaderIcon
+        size={25}
+        margin={5}
+        source={require('../assets/mail.png')}
+        onPress={props.onPress}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex-direction: row;
+  background-color: transparent;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 18px;
+  color: #ffffff;
+`;

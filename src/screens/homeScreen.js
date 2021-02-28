eb99e5bab321d7604ea8b381e1ce67dba61e85f6
Samuel Olwe <sam.olwe@gmail.com>
2021-02-28 11:20:44 +0300

    Integrated google maps sdk

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,36 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useWindowDimensions} from 'react-native';
-import MapboxGL from '@react-native-mapbox-gl/maps';
+import MapView from 'react-native-maps';
 
-MapboxGL.setAccessToken(
-  'pk.eyJ1Ijoic2Ftb2x3ZSIsImEiOiJja2t4aGsxNHYyZzl1MnFueW1zMjZ4MWVpIn0.D288xM5ZDihIhNBDiS2d8w',
-);
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    flex: 1,
-  },
-});
-
-export default function SignInScreen({navigation}) {
+export default function HomeScreen({navigation}) {
   const window = useWindowDimensions();
   return (
-    <View style={styles.page}>
-      <View
-        style={{
-          height: window.height,
-          width: window.width,
-          backgroundColor: 'grey',
-        }}>
-        <MapboxGL.MapView style={styles.map} />
-      </View>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});

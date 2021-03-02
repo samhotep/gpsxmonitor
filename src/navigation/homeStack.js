da3004f3361d53682b74997b68c52b50e82744c0
Samuel Olwe <sam.olwe@gmail.com>
2021-03-02 03:58:00 +0300

    Changed to navigation prop

--- src/navigation/homeStack.js
+++ src/navigation/homeStack.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/homeScreen';
 import HeaderIcon from '../components/headers/headerIcon';

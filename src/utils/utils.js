25e8080395e6a30fbb7f8fed0759e81ca9ea33ee
Samuel Olwe <sam.olwe@gmail.com>
2021-03-03 23:20:21 +0300

    Create util for sorting categories

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,19 @@
+/**
+ * Reusable utility functional objects
+ */
+
 const Utils = {
-  getCategories: (item) => {},
+  /**
+   * Return modified array with objects sorted into categories, which are
+   * mapped with identifier colors
+   */
+  getCategories: (list) => {
+    let cc = [];
+    list.map((_, i) => {
+      cc[_.group_id] = _.group_id;
+    });
+    return cc;
+  },
 };
 
 export default Utils;

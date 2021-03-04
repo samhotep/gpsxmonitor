a766f803e4b0ebb62fa60614cd4ecab6d49b0663
Samuel Olwe <sam.olwe@gmail.com>
2021-03-04 20:15:06 +0300

    Removed inherited ref

--- src/components/inputs/input.js
+++ src/components/inputs/input.js
@@ -1,31 +1,32 @@
-import React, {forwardRef} from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components/native';
 
-const Input = forwardRef((props, ref) => (
-  <Container
-    bgcolor={props.bgcolor}
-    margin={props.margin}
-    noBorder={props.noBorder}>
-    <FormInput
-      ref={ref}
-      defaultValue={props.defaultValue}
-      placeholder={props.placeholder}
-      secureTextEntry={props.hidden}
-      autoCompleteType={props.autoCompleteType}
-      keyboardType={props.keyboardType}
-      blurOnSubmit={true}
-      clearTextOnFocus={true}
-      editable={props.editable}
-      width={props.width}
-      maxLength={props.maxLength}
-      onChangeText={props.onChangeText}
-      selectionColor={props.selectionColor}
-      value={props.value}
-      font={props.font}
-      color={props.color}
-    />
-  </Container>
-));
+export default function Input(props) {
+  return (
+    <Container
+      bgcolor={props.bgcolor}
+      margin={props.margin}
+      noBorder={props.noBorder}>
+      <FormInput
+        defaultValue={props.defaultValue}
+        placeholder={props.placeholder}
+        secureTextEntry={props.hidden}
+        autoCompleteType={props.autoCompleteType}
+        keyboardType={props.keyboardType}
+        blurOnSubmit={true}
+        clearTextOnFocus={true}
+        editable={props.editable}
+        width={props.width}
+        maxLength={props.maxLength}
+        onChangeText={props.onChangeText}
+        selectionColor={props.selectionColor}
+        value={props.value}
+        font={props.font}
+        color={props.color}
+      />
+    </Container>
+  );
+}
 
 const FormInput = styled.TextInput`
   font-size: ${(props) => props.font || 20}px;
@@ -47,5 +48,3 @@ const Container = styled.View`
   border-bottom-width: ${(props) => (props.noBorder ? 0 : 1)}px;
   border-bottom-color: #c5c5c5;
 `;
-
-export default Input;

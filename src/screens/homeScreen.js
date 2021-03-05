89c6d94d8bce185ad29ce329d13d32bf149b8e7f
Samuel Olwe <sam.olwe@gmail.com>
2021-03-05 16:07:53 +0300

    Added updateable prop for region

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -14,6 +14,7 @@ const eventEmitter = new NativeEventEmitter(NativeModules.ToastExample);
 // TODO Pass the location as a state prop, or as an event emitter
 export default function HomeScreen({navigation}) {
   const window = useWindowDimensions();
+  const [currentTracker, setCurrentTracker] = useState();
   const [location, setLocation] = useState({
     latitude: 2.6031808853,
     longitude: 31.9491958618,
@@ -22,6 +23,7 @@ export default function HomeScreen({navigation}) {
   });
 
   const updateLocation = (newCoordinates) => {
+    console.log('New Location Received');
     setLocation(newCoordinates);
   };
 
@@ -29,8 +31,20 @@ export default function HomeScreen({navigation}) {
     // Listener for location update events
     const eventListener = eventEmitter.addListener(
       'event.trackerEvent',
-      (coordinates) => {
-        updateLocation(coordinates);
+      (trackerData) => {
+        setCurrentTracker(trackerData);
+        updateLocation({
+          latitude: trackerData.gps.location.lat,
+          longitude: trackerData.gps.location.lng,
+          latitudeDelta: 0,
+          longitudeDelta: 0,
+        });
+        console.log({
+          latitude: trackerData.gps.location.lat,
+          longitude: trackerData.gps.location.lng,
+          latitudeDelta: 0,
+          longitudeDelta: 0,
+        });
       },
     );
     return () => {
@@ -49,6 +63,7 @@ export default function HomeScreen({navigation}) {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        region={location}
       />
     </View>
   );
@@ -64,3 +79,36 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
+
+let p = {
+  actual_track_update: '2020-08-31 20:18:40',
+  additional: {
+    external_power_state: {updated: '2021-03-05 15:58:40', value: '1'},
+    gps_antenna_state: {updated: '2021-03-05 15:58:40', value: '1'},
+    gsm_damp_state: {updated: '2021-03-05 15:58:40', value: '0'},
+  },
+  battery_level: 0,
+  battery_update: '2021-03-05 15:58:40',
+  connection_status: 'active',
+  gps: {
+    alt: 1119,
+    heading: 0,
+    location: {lat: 2.238262891769409, lng: 32.89451217651367},
+    signal_level: 100,
+    speed: 0,
+    updated: '2021-03-05 15:58:40',
+  },
+  gsm: {
+    network_name: 'MTN',
+    roaming: null,
+    signal_level: 100,
+    updated: '2021-03-05 15:58:40',
+  },
+  inputs: [false, false, false, false, false],
+  inputs_update: '2021-03-05 15:58:40',
+  last_update: '2021-03-05 15:59:35',
+  movement_status: 'parked',
+  outputs: [false, false, false],
+  outputs_update: '2021-03-05 15:58:40',
+  source_id: 991,
+};

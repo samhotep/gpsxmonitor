bdf63caeafd1c6e511e6ad354e2d2cad2cb93c13
Samuel Olwe <sam.olwe@gmail.com>
2021-03-08 16:35:20 +0300

    Show icon depeding on format

--- src/components/items/homeItem.js
+++ src/components/items/homeItem.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 import Utils from '../../utils/utils';
 
 // TODO Animate the list
+// TODO Show time in days, mins, hours
 export default function HomeItem(props) {
   const [signalIcon, setSignalIcon] = useState();
   const [timeStatus, setTimeStatus] = useState('since today');
+  const [movementIcon, setMovementIcon] = useState(
+    require('../../assets/moving.png'),
+  );
 
   const getSignalIcon = () => {
     if (props.signal >= 75) {
@@ -19,7 +23,18 @@ export default function HomeItem(props) {
     }
   };
 
+  const getMovementIcon = () => {
+    if (props.movement === 'moving') {
+      setMovementIcon(require('../../assets/moving.png'));
+    } else if (props.movement === 'parked') {
+      setMovementIcon(require('../../assets/parked.png'));
+    } else if (props.movement === 'stopped') {
+      setMovementIcon(require('../../assets/stop.png'));
+    }
+  };
+
   useEffect(() => {
+    getMovementIcon();
     getSignalIcon();
     setTimeStatus(Utils.getTimeDifference(props.time));
   }, [props]);
@@ -37,19 +52,21 @@ export default function HomeItem(props) {
           </Text>
         </RowContainer>
         <RowContainer>
-          <Text size={14} color="#b9b9b9">
+          <Text size={14} color="#b9b9b9" width={100}>
             Signal: {props.signal}%
           </Text>
+          <ImageContainer source={movementIcon} size={24} margin={1} />
+          <TimeLabel size={12}>
+            {props.movement.charAt(0).toUpperCase() + props.movement.slice(1)}{' '}
+            for 21 min
+          </TimeLabel>
         </RowContainer>
       </ColumnContainer>
       <ColumnContainer>
         <RowContainer>
-          <Text size={14} color="#b9b9b9">
+          <Text size={14} width={100} color="#b9b9b9">
             {timeStatus} min. ago
           </Text>
-        </RowContainer>
-        <RowContainer>
-          <Text size={14}>{props.movement}</Text>
         </RowContainer>
       </ColumnContainer>
     </Container>
@@ -72,19 +89,19 @@ const RowContainer = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  margin: 5px;
+  margin: ${(props) => props.margin || 5}px;
 `;
 
 const ColumnContainer = styled.View`
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 `;
 
 const ImageContainer = styled.Image`
   width: ${(props) => props.size || 24}px;
   height: ${(props) => props.size || 24}px;
-  margin: 5px;
+  margin: ${(props) => props.margin || 5}px;
 `;
 
 const Text = styled.Text.attrs({
@@ -94,6 +111,13 @@ const Text = styled.Text.attrs({
   font-size: ${(props) => props.size || 18}px;
   color: ${(props) => props.color || '#636261'};
   width: ${(props) => props.width || 220}px;
+`;
+
+const TimeLabel = styled.Text`
+  text-align: left;
+  font-size: ${(props) => props.size || 18}px;
+  color: #636261;
+  width: ${(props) => props.width || 100}px;
 `;
 
 let v = {

152d356ccbabf0e7e9fd819e2a32b7cf363d29fd
Samuel Olwe <sam.olwe@gmail.com>
2021-03-09 05:00:58 +0300

    Fix wrong prop name bug

--- src/components/items/homeItem.js
+++ src/components/items/homeItem.js
@@ -14,23 +14,30 @@ export default function HomeItem(props) {
   const [movementStatus, setMovementStatus] = useState('0');
 
   const getSignalIcon = () => {
-    if (props.signal >= 75) {
+    console.log(props.tracker.gps.signal_level);
+    if (props.tracker.gps.signal_level >= 75) {
       setSignalIcon(require('../../assets/signal/strong.png'));
-    } else if (props.signal >= 50 && props.signal < 75) {
+    } else if (
+      props.tracker.gps.signal_level >= 50 &&
+      props.tracker.gps.signal_level < 75
+    ) {
       setSignalIcon(require('../../assets/signal/good.png'));
-    } else if (props.signal >= 25 && props.signal < 50) {
+    } else if (
+      props.tracker.gps.signal_level >= 25 &&
+      props.tracker.gps.signal_level < 50
+    ) {
       setSignalIcon(require('../../assets/signal/low.png'));
-    } else if (props.signal < 25) {
+    } else if (props.tracker.gps.signal_level < 25) {
       setSignalIcon(require('../../assets/signal/poor.png'));
     }
   };
 
   const getMovementIcon = () => {
-    if (props.movement === 'moving') {
+    if (props.tracker.movement_status === 'moving') {
       setMovementIcon(require('../../assets/moving.png'));
-    } else if (props.movement === 'parked') {
+    } else if (props.tracker.movement_status === 'parked') {
       setMovementIcon(require('../../assets/parked.png'));
-    } else if (props.movement === 'stopped') {
+    } else if (props.tracker.movement_status === 'stopped') {
       setMovementIcon(require('../../assets/stop.png'));
     }
   };
@@ -46,46 +53,52 @@ export default function HomeItem(props) {
 
   return (
     <Container onPress={props.onPress} selected={props.selected}>
-      <ColumnContainer>
-        <ImageContainer source={require('../../assets/arrow.png')} size={24} />
-        <ImageContainer source={signalIcon} resizeMode="contain" size={16} />
-      </ColumnContainer>
-      <ColumnContainer>
-        <RowContainer>
+      <RowContainer>
+        <InnerContainer>
+          <ImageContainer
+            source={require('../../assets/arrow.png')}
+            size={24}
+          />
           <Text selected={props.selected} color="#202020">
             {props.tracker.label}
           </Text>
-        </RowContainer>
-        <RowContainer>
+        </InnerContainer>
+        <TimeLabel size={14} width={80} color="#b9b9b9">
+          {timeStatus} ago
+        </TimeLabel>
+      </RowContainer>
+      <RowContainer>
+        <InnerContainer>
+          <ImageContainer
+            source={signalIcon}
+            resizeMode="contain"
+            size={16}
+            margin={10}
+          />
           <Text size={14} color="#b9b9b9" width={100}>
             Signal: {props.tracker.gps.signal_level}%
           </Text>
+        </InnerContainer>
+        <InnerContainer>
           <ImageContainer source={movementIcon} size={24} margin={1} />
-          <TimeLabel size={12}>
+          <TimeLabel size={12} width={150}>
             {props.tracker.movement_status.charAt(0).toUpperCase() +
               props.tracker.movement_status.slice(1)}{' '}
             for {movementStatus}
           </TimeLabel>
-        </RowContainer>
-      </ColumnContainer>
-      <ColumnContainer>
-        <RowContainer>
-          <Text size={14} width={100} color="#b9b9b9">
-            {timeStatus} ago
-          </Text>
-        </RowContainer>
-      </ColumnContainer>
+        </InnerContainer>
+      </RowContainer>
     </Container>
   );
 }
 
 const Container = styled.TouchableOpacity`
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
   background-color: #ffffff;
   width: 100%;
-  padding: 10px;
+  padding: 5px;
   position: absolute;
   top: 0px;
   elevation: 2;
@@ -94,13 +107,14 @@ const Container = styled.TouchableOpacity`
 const RowContainer = styled.View`
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   margin: ${(props) => props.margin || 5}px;
+  width: 100%;
 `;
 
-const ColumnContainer = styled.View`
-  flex-direction: column;
-  align-items: flex-start;
+const InnerContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
   justify-content: center;
 `;
 
@@ -122,40 +136,6 @@ const Text = styled.Text.attrs({
 const TimeLabel = styled.Text`
   text-align: left;
   font-size: ${(props) => props.size || 18}px;
-  color: #636261;
+  color: ${(props) => props.color || '#636261'};
   width: ${(props) => props.width || 100}px;
 `;
-
-let v = {
-  actual_track_update: '2020-11-16 12:46:58',
-  additional: {
-    external_power_state: {updated: '2021-03-06 17:51:42', value: '1'},
-    gps_antenna_state: {updated: '2021-03-06 17:51:42', value: '1'},
-    gsm_damp_state: {updated: '2021-03-06 17:51:42', value: '0'},
-  },
-  battery_level: 100,
-  battery_update: '2021-03-06 17:51:42',
-  connection_status: 'active',
-  gps: {
-    alt: 616,
-    heading: 0,
-    location: {lat: 3.569007635116577, lng: 32.07025909423828},
-    signal_level: 100,
-    speed: 0,
-    updated: '2021-03-06 17:51:42',
-  },
-  gsm: {
-    network_name: 'MTN',
-    roaming: null,
-    signal_level: 54,
-    updated: '2021-03-06 17:51:42',
-  },
-  inputs: [true, false, false, false, false],
-  inputs_update: '2021-03-06 17:51:42',
-  label: '2010093 - URA ELEGU GENSET',
-  last_update: '2021-03-06 17:52:21',
-  movement_status: 'parked',
-  outputs: [false, false, false],
-  outputs_update: '2021-03-06 17:51:42',
-  source_id: 992,
-};

ad3b4a4e59c188cd68ef13fbcfc25b130a73c955
Samuel Olwe <sam.olwe@gmail.com>
2021-04-10 06:06:18 +0300

    Added payment endpoint

--- src/screens/confirmationScreen.js
+++ src/screens/confirmationScreen.js
@@ -3,10 +3,13 @@ import {StatusBar, ToastAndroid} from 'react-native';
 import styled from 'styled-components';
 import BillingButton from '../components/buttons/billingButton';
 import Input from '../components/inputs/input';
+import FloatingLoader from '../components/loaders/floatingLoader';
+import API from '../api/api';
 
 // TODO Pass the location as a state prop, or as an event emitter
 export default function ConfirmationScreen({route, navigation}) {
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [loading, setLoading] = useState(false);
   const service = route.params;
   const periods = {
     1: {name: 'Day', time: '1 day'},
@@ -20,6 +23,68 @@ export default function ConfirmationScreen({route, navigation}) {
         ToastAndroid.SHORT,
         ToastAndroid.CENTER,
       );
+    } else {
+      setLoading(true);
+      API.subscribe(phoneNumber, service.serviceId)
+        .then((result) => {
+          let x = {
+            code: 0,
+            data: {
+              amount: 10000,
+              ceatedOn: '2021-04-10T02:57:37.2847008Z',
+              errorMessage: null,
+              errorMessageCode: null,
+              productId: 'e26303f32d0e4b16890bbf44de8f257c',
+              recipientNumber: '256785016759',
+              service: null,
+              serviceId: null,
+              status: 'OK',
+              statusCode: '1',
+              statusMessage: null,
+              transactionId: '6bcece127b52471692ec6e49a3768625',
+              transactionReference:
+                'NqBVuOMrW1PYrgdLDqIA5LbsHKf7bE8apOUfOCz96t0pzq2CxZfmco621fkRl499c3c8950bbbba686c3fe385ddff2abb66',
+              transactionStatus: 'PENDING',
+              updatedBy: null,
+              updatedOn: '0001-01-01T00:00:00',
+              user: {
+                apiKey: null,
+                createdOn: '2021-04-09T23:56:30',
+                email: 'test@fms-ecsafrica.com',
+                userId: 'b33802f8d1fc4ff4921bca9833a69dd8',
+              },
+              userId: 'b33802f8d1fc4ff4921bca9833a69dd8',
+              validateTillDate: '0001-01-01T00:00:00',
+            },
+            message: 'Transaction recorded',
+          };
+          console.log(result);
+          if (result.code === -1) {
+            ToastAndroid.show(
+              result.message,
+              ToastAndroid.SHORT,
+              ToastAndroid.CENTER,
+            );
+          } else if (result.code === 0) {
+            navigation.navigate('Success', result.data);
+          } else {
+            ToastAndroid.show(
+              result.message,
+              ToastAndroid.SHORT,
+              ToastAndroid.CENTER,
+            );
+          }
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          ToastAndroid.show(
+            'Network request failed',
+            ToastAndroid.SHORT,
+            ToastAndroid.CENTER,
+          );
+          setLoading(false);
+        });
     }
   };
 
@@ -75,6 +140,7 @@ export default function ConfirmationScreen({route, navigation}) {
         }}
       />
       <BillingButton title="Confirm" onPress={submitRequest} />
+      {loading ? <FloatingLoader /> : null}
     </Container>
   );
 }

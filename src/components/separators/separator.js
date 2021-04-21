01036523180b2daa17a87ace3842d6c81bce2145
Samuel Olwe <sam.olwe@gmail.com>
2021-04-21 18:10:08 +0300

    Created separator

---
+++ src/components/separators/separator.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+const Separator = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  height: 1px;
+  width: 100%;
+  margin: 5px;
+  background-color: #808080;
+`;
+
+export default Separator;

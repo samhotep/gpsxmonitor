b99ed92c1857a6c39f8dd0f86cb84394bc3cc4f3
Samuel Olwe <sam.olwe@gmail.com>
2021-05-03 02:10:43 +0300

    Resize radio inputs

--- src/components/inputs/radioInput.js
+++ src/components/inputs/radioInput.js
@@ -16,8 +16,8 @@ export default function RadioInput(props) {
 
 const Container = styled.TouchableOpacity`
   border-radius: 30px;
-  height: ${(props) => props.size || 24}px;
-  width: ${(props) => props.size || 24}px;
+  height: ${(props) => props.size || 21}px;
+  width: ${(props) => props.size || 21}px;
   margin: ${(props) => props.margin || 5}px;
   padding: 4px;
   border: 2px ${(props) => props.color || '#000000'};

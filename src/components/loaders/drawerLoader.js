e4edb7848f4b9fe3b494332f299fef34fbfde982
Samuel Olwe <sam.olwe@gmail.com>
2021-05-04 21:21:44 +0300

    Resize drawer loader

--- src/components/loaders/drawerLoader.js
+++ src/components/loaders/drawerLoader.js
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import {useWindowDimensions} from 'react-native';
 import styled from 'styled-components';
 
 export default function DrawerLoader() {
-  const window = useWindowDimensions();
   const size = 50;
-  const depth = 100;
 
   return (
     <Container size={size}>
-      <Loader size={90} color="#4788c7" />
+      <Loader size={70} color="#4788c7" />
     </Container>
   );
 }
@@ -19,7 +16,7 @@ const Container = styled.View`
   align-items: center;
   justify-content: center;
   border-radius: 25px;
-  background-color: #ffffff;
+  background-color: transparent;
 `;
 
 const Loader = styled.ActivityIndicator``;

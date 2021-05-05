46154aa2af1905b0c52c04d3f7c6d8fb58b55db0
Samuel Olwe <sam.olwe@gmail.com>
2021-05-05 04:51:34 +0300

    Import settings screen

--- src/navigation/mainStack.js
+++ src/navigation/mainStack.js
@@ -1,16 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import {ToastAndroid, useWindowDimensions} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import styled from 'styled-components';
-import CheckBox from '@react-native-community/checkbox';
 import AuthStack from './authStack';
-import HeaderTitle from '../components/headers/headerTitle';
-import FloatingLoader from '../components/loaders/floatingLoader';
-import Storage from '../storage/storage';
-import API from '../api/api';
 import Dashboard from './dashboard';
+import SettingsScreen from '../screens/settingsScreen';
+import FloatingLoader from '../components/loaders/floatingLoader';
+import API from '../api/api';
 
 const Stack = createStackNavigator();
 
@@ -27,126 +23,13 @@ function SettingsDrawer({route, navigation}) {
         backgroundColor: '#ffffff',
         width: window.width,
       }}
-      drawerContent={(props) => <CustomDrawerContent {...props} />}>
+      drawerContent={(props) => <SettingsScreen {...props} />}>
       <Drawer.Screen
         name="Auth"
         component={AuthStack}
         options={{headerShown: false}}
       />
     </Drawer.Navigator>
-  );
-}
-
-function CustomDrawerContent(props) {
-  const navigation = useNavigation();
-  const [sortObjects, setSortObjects] = useState(false);
-  const [sortValue, setSortValue] = useState('disabled');
-  const [labelsEnabled, setLabelsEnabled] = useState(false);
-  const [inputURL, setInputURL] = useState('');
-
-  const headerItems = [
-    {
-      label: 'Notification settings',
-      item: (
-        <Button>
-          <ItemLabel color="#202020" size={18} margin={5}>
-            Notifications
-          </ItemLabel>
-        </Button>
-      ),
-    },
-    {
-      label: 'Map Settings',
-      item: (
-        <InputRowContainer>
-          <InputContainer>
-            <ItemLabel color="#202020" size={18}>
-              Object Labels
-            </ItemLabel>
-            <ItemLabel color="#acacac" size={16}>
-              Object labels displayed on map
-            </ItemLabel>
-          </InputContainer>
-          <CheckBox
-            disabled={false}
-            value={labelsEnabled}
-            onValueChange={() =>
-              labelsEnabled ? setLabelsEnabled(false) : setLabelsEnabled(true)
-            }
-            tintColors={{true: '#1e96dc', false: '#1e96dc'}}
-          />
-        </InputRowContainer>
-      ),
-    },
-    {
-      label: 'Sorting settings',
-      item: (
-        <Button onPress={toggleSortObjects}>
-          <InputContainer>
-            <ItemLabel color="#202020" size={18}>
-              Sort objects
-            </ItemLabel>
-            <ItemLabel color="#acacac" size={16}>
-              {sortValue}
-            </ItemLabel>
-          </InputContainer>
-        </Button>
-      ),
-    },
-    {
-      label: 'Advanced settings',
-      item: (
-        <InputContainer>
-          <ItemLabel color="#202020" size={18}>
-            API server URL
-          </ItemLabel>
-          <FormInput
-            placeholder="https://hosting.fms-ecsinternational.com/api/"
-            size={16}
-            value={inputURL}
-            onChangeText={(value) => {
-              setInputURL(value);
-              Storage.setURL(value);
-            }}
-          />
-        </InputContainer>
-      ),
-    },
-  ];
-
-  const toggleSortObjects = () => {
-    if (sortObjects) {
-      setSortValue('disabled');
-    } else {
-      setSortValue('enabled');
-    }
-    setSortObjects(!sortObjects);
-  };
-
-  return (
-    <DrawerContainer>
-      <HeaderTitle
-        source={require('../assets/back.png')}
-        onPress={() => {
-          navigation.goBack();
-        }}
-        label="Settings"
-      />
-      {headerItems.map((_, i) => {
-        return <HeaderItem key={_.label} label={_.label} item={_.item} />;
-      })}
-    </DrawerContainer>
-  );
-}
-
-function HeaderItem(props) {
-  return (
-    <ItemContainer>
-      <ItemLabel size={16} margin={5}>
-        {props.label}
-      </ItemLabel>
-      {props.item}
-    </ItemContainer>
   );
 }
 
@@ -212,49 +95,3 @@ export default function MainStack({route, navigation}) {
     </Stack.Navigator>
   );
 }
-
-const DrawerContainer = styled.ScrollView`
-  flex-direction: column;
-`;
-
-const ItemContainer = styled.View`
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  padding: 10px;
-  width: 100%;
-`;
-
-const ItemLabel = styled.Text`
-  font-size: ${(props) => props.size || 20}px;
-  color: ${(props) => props.color || '#4788c7'};
-  font-family: 'Roboto-Regular';
-  padding: ${(props) => props.padding || 0}px;
-  margin: ${(props) => props.margin || 0}px;
-  width: 100%;
-`;
-
-const Button = styled.TouchableOpacity`
-  width: 100%;
-`;
-
-const FormInput = styled.TextInput`
-  font-size: ${(props) => props.size || 22}px;
-  color: #333333;
-  width: 100%;
-  text-align: left;
-  padding: 0px;
-`;
-
-const InputContainer = styled.View`
-  flex-direction: column;
-  justify-content: space-around;
-  width: 100%;
-  margin: 5px;
-`;
-
-const InputRowContainer = styled.View`
-  flex-direction: row;
-  justify-content: center;
-  padding: 0 20px 0 20px;
-`;

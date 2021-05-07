4b70dd5e0a70b967a1c485b316cd3cfa8241cf52
Samuel Olwe <sam.olwe@gmail.com>
2021-05-08 00:51:27 +0300

    Added list for input type

--- src/components/lists/lists.js
+++ src/components/lists/lists.js
@@ -6,6 +6,40 @@ const lists = {
     signal_lost: {color: '#fc6701', text: 'Connection lost'},
     offline: {color: '#fc6701', text: 'Offline'},
   },
+  inputTypes: {
+    ignition: {
+      false: require('../../assets/flash.png'),
+      true: require('../../assets/flash_active.png'),
+    },
+    engine: {
+      false: require('../../assets/engine.png'),
+      true: require('../../assets/engine_active.png'),
+    },
+    mass: {
+      false: require('../../assets/omega.png'),
+      true: require('../../assets/omega_active.png'),
+    },
+    car_alarm: {
+      false: require('../../assets/alarm.png'),
+      true: require('../../assets/alarm_active.png'),
+    },
+    sos_button: {
+      false: require('../../assets/emergency.png'),
+      true: require('../../assets/emergency_active.png'),
+    },
+    hood: {
+      false: require('../../assets/hood.png'),
+      true: require('../../assets/hood_active.png'),
+    },
+    door: {
+      false: require('../../assets/door.png'),
+      true: require('../../assets/door_active.png'),
+    },
+    car_lock: {
+      false: require('../../assets/lock.png'),
+      true: require('../../assets/lock_active.png'),
+    },
+  },
 };
 
 export default lists;

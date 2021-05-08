18a19e33306376210bf1a53659a53bf7ff24e350
Samuel Olwe <sam.olwe@gmail.com>
2021-05-08 23:57:18 +0300

    Refactor

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -55,11 +55,7 @@ const Utils = {
         }
         millis = millis % _.length;
       });
-      if (destructuredTime === '') {
-        return 'Now';
-      } else {
-        return destructuredTime;
-      }
+      return destructuredTime;
     } else {
       return '';
     }

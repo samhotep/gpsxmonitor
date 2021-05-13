62dce39e6c68b230c8bd3959cdbccef96515b9e1
Samuel Olwe <sam.olwe@gmail.com>
2021-05-13 23:43:19 +0300

    Get track prop

--- src/components/items/eventItem.js
+++ src/components/items/eventItem.js
@@ -8,14 +8,30 @@ export default function EventItem(props) {
         <ImageContainer source={require('../../assets/track.png')} />
       </RowContainer>
       <RowContainer>
-        <Text selected={props.selected}>{props.text}</Text>
+        <Text>{props.track.start_address}</Text>
       </RowContainer>
     </Container>
   );
 }
 
+// {
+//     id: 123456,
+//     start_date: '2020-09-23 03:39:44',
+//     start_address: '1255 6th Ave, New York, NY 10020, USA',
+//     max_speed: 62,
+//     end_date: '2020-09-23 06:39:44',
+//     end_address: '888 5th Ave, New York, NY 10021, USA',
+//     length: 5.5,
+//     points: 327,
+//     avg_speed: 49,
+//     event_count: 3,
+//     norm_fuel_consumed: 1.07,
+//     type: 'regular',
+//     gsm_lbs: false,
+//   },
+
 const Container = styled.View`
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: space-between;
   background-color: #ffffff;
@@ -31,8 +47,8 @@ const RowContainer = styled.View`
 `;
 
 const ImageContainer = styled.Image`
-  height: ${(props) => props.size || 24}px;
-  width: ${(props) => props.size || 24}px;
+  height: ${(props) => props.size || 48}px;
+  width: ${(props) => props.size || 48}px;
 `;
 
 const Text = styled.Text`

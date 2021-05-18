7c00d7c3b3f1f9f58590587248cd309928ab928b
Samuel Olwe <sam.olwe@gmail.com>
2021-05-19 00:01:00 +0300

    Updated format

--- src/components/items/eventItem.js
+++ src/components/items/eventItem.js
@@ -1,23 +1,38 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import Separator from '../separators/separator';
 import styled from 'styled-components';
 
 export default function EventItem(props) {
+  const [statusDate, setStatusDate] = useState('');
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    let date = new Date(props.event.status_change_date.replace(/-+/g, '/'));
+    setStatusDate(
+      `${date.toLocaleDateString([], {
+        year: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+      })} ${date.toLocaleTimeString([], {
+        year: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+      })}`,
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Container onPress={props.onPress}>
         <ImageContainer source={require('../../assets/bell_blue.png')} />
         <ColumnContainer>
-          <RowContainer>
-            <Text>{props.event.location.address}</Text>
-          </RowContainer>
-
-          <DetailsContainer>
-            <Text>{`${props.event.status_change_date.replace(
-              /-+/g,
-              '/',
-            )}`}</Text>
-          </DetailsContainer>
+          <Text color="#737373">
+            Rahul (Kamaz): Rahul Adenaur: task: "{props.event.description}"{' '}
+            {props.event.status.toUpperCase()}
+          </Text>
+          <Text align="right">{statusDate.slice(0, -3)}</Text>
         </ColumnContainer>
       </Container>
       <Separator nomargin />
@@ -31,45 +46,26 @@ const Container = styled.TouchableOpacity`
   justify-content: flex-start;
   background-color: #ffffff;
   padding: 5px;
-  width: 100%;
-`;
-
-const RowContainer = styled.View`
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: flex-start;
 `;
 
 const ColumnContainer = styled.View`
+  flex: 1;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: flex-start;
-  width: 90%;
+  justify-content: center;
 `;
 
 const ImageContainer = styled.Image`
-  height: ${(props) => props.size || 24}px;
-  width: ${(props) => props.size || 24}px;
-`;
-
-const DetailsContainer = styled.View`
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: space-between;
-  width: 90%;
+  height: ${(props) => props.size || 15}px;
+  width: ${(props) => props.size || 15}px;
+  margin: 5px;
 `;
 
 const Text = styled.Text`
-  text-align: left;
+  text-align: ${(props) => props.align || 'left'};
   flex-wrap: wrap;
   font-size: 14px;
-  font-weight: 700;
-  color: ${(props) => (props.selected ? '#ffffff' : '#a8a8a8')};
-  width: 80%;
-`;
-
-const BoldText = styled.Text`
-  font-size: 14px;
-  font-weight: bold;
-  color: ${(props) => props.color || '#737373'};
+  color: ${(props) => props.color || '#a8a8a8'};
+  width: 250px;
+  padding: 2px;
 `;

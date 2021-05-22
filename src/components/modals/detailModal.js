51de66be3e739154f3a3d0661367513abfabfec8
Samuel Olwe <sam.olwe@gmail.com>
2021-05-22 19:21:50 +0300

    Created modals for the details screen

---
+++ src/components/modals/detailModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function DetailModal(props) {
+  return (
+    !props.clicked || (
+      <Container
+        top={props.top}
+        right={props.right}
+        height={props.height}
+        width={props.width}>
+        {props.inject}
+      </Container>
+    )
+  );
+}
+
+const Container = styled.View`
+  position: absolute;
+  background-color: #ffffff;
+  border-radius: 3px;
+  height: ${(props) => props.height || 150}px;
+  width: ${(props) => props.width || 100}px;
+  elevation: 4;
+  top: ${(props) => props.top || 30}px;
+  right: ${(props) => props.right || 10}px;
+`;

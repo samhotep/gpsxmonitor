03bb75b0f408c72b45656a4e91fc0d672beeb28c
Samuel Olwe <sam.olwe@gmail.com>
2021-05-26 21:58:48 +0300

    Created task item

---
+++ src/screens/taskItem.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+export default function TaskItem(props) {
+  return (
+    <Highlight onPress={props.onPress}>
+      <Container selected={props.selected}>
+        <Text selected={props.selected}>{props.text}</Text>
+        <IdentityColor color={props.color} />
+      </Container>
+    </Highlight>
+  );
+}
+
+const Highlight = styled.TouchableHighlight``;
+
+const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${(props) => (props.selected ? '#4788c7' : '#ffffff')};
+  width: 100%;
+  padding: 10px;
+`;
+
+const IdentityColor = styled.View`
+  align-items: flex-start;
+  justify-content: center;
+  background-color: ${(props) => props.color || '#4788c7'};
+  border-radius: 15px;
+  border: 1px #ffffff;
+  width: 8px;
+  padding: 7px;
+  margin-right: 5px;
+`;
+
+const Text = styled.Text`
+  flex: 1;
+  text-align: left;
+  flex-wrap: wrap;
+  font-size: 14px;
+  color: ${(props) => (props.selected ? '#ffffff' : '#202020')};
+`;

18d3ae3d05d6f4f35f61130205784848dc7c5dd0
Samuel Olwe <sam.olwe@gmail.com>
2021-05-27 23:20:50 +0300

    Created an asignee screen

---
+++ src/screens/assigneeScreen.js
@@ -0,0 +1,93 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import {StatusBar, ToastAndroid} from 'react-native';
+import Separator from '../components/separators/separator';
+import styled from 'styled-components';
+import DrawerLoader from '../components/loaders/drawerLoader';
+import Storage from '../storage/storage';
+
+export default function AssigneeScreen({route, navigation}) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {}, []);
+
+  if (loading) {
+    return (
+      <Container>
+        <StatusBar backgroundColor="#007aa6" />
+        <DrawerLoader />
+      </Container>
+    );
+  }
+
+  return <Container></Container>;
+}
+
+const Container = styled.Pressable`
+  flex-direction: column;
+  background-color: #ffffff;
+  height: 100%;
+`;
+
+const ContentContainer = styled.ScrollView`
+  flex-direction: column;
+  width: 100%;
+  padding: 10px;
+`;
+
+const FilterContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+  width: 100%;
+  height: 50px;
+  padding: 4px;
+  elevation: 3;
+`;
+
+const TimeContainer = styled.TouchableOpacity`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 10px 5px 10px;
+`;
+
+const OptionContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin: 5px;
+`;
+
+const Text = styled.Text`
+  font-size: ${(props) => props.size || 18}px;
+  color: ${(props) => props.color || '#000000'};
+  font-family: 'Roboto-Regular';
+  font-weight: ${(props) => props.weight || 'normal'};
+  text-align: ${(props) => props.align || 'left'};
+  margin: ${(props) => props.margin || 5}px;
+`;
+
+const ImageContainer = styled.Image`
+  height: ${(props) => props.size || 24}px;
+  width: ${(props) => props.size || 24}px;
+`;
+
+const ModalContainer = styled.View`
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 5px;
+`;
+
+const RadioContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.size || 28}px;
+  height: ${(props) => props.size || 28}px;
+  margin: ${(props) => props.margin || 10}px;
+`;

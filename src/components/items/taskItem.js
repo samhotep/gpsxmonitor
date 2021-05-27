1e7f6f69c0ea3b1ebb882d87e02d187d438a7ca1
Samuel Olwe <sam.olwe@gmail.com>
2021-05-27 23:19:48 +0300

    Created task item

---
+++ src/components/items/taskItem.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+import Separator from '../separators/separator';
+import styled from 'styled-components';
+
+export default function TaskItem(props) {
+  return (
+    <Container>
+      <RowContainer>
+        <ImageContainer source={require('../../assets/board.png')} />
+        <Text>{props.group.tracker.label}</Text>
+      </RowContainer>
+      <Separator />
+      <RowContainer>
+        <ImageContainer source={require('../../assets/circle.png')} size={24} />
+        <Text size={22}>{props.group.task.label}</Text>
+      </RowContainer>
+      <RowContainer>
+        <ImageContainer
+          source={require('../../assets/calendar.png')}
+          size={24}
+        />
+        <Text color="#737373">
+          {props.group.task.from} {props.group.task.to}
+        </Text>
+      </RowContainer>
+      <RowContainer>
+        <ImageContainer source={require('../../assets/pin.png')} size={24} />
+        <Text color="#737373">{props.group.task.location.address}</Text>
+      </RowContainer>
+      <Separator />
+    </Container>
+  );
+}
+
+const Container = styled.TouchableOpacity`
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #ffffff;
+  width: 100%;
+`;
+
+const RowContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0px 20px 0px 20px;
+`;
+
+const ImageContainer = styled.Image`
+  height: ${(props) => props.size || 32}px;
+  width: ${(props) => props.size || 32}px;
+`;
+
+const Text = styled.Text`
+  flex: 1;
+  text-align: left;
+  flex-wrap: wrap;
+  font-size: ${(props) => props.size || 14}px;
+  color: ${(props) => (props.selected ? '#ffffff' : '#202020')};
+  margin: 10px;
+`;

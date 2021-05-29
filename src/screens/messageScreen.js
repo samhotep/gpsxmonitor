50e309d6cb1d1b3854a0dee30752bb9acaf8e214
Samuel Olwe <sam.olwe@gmail.com>
2021-05-30 02:55:13 +0300

    Refactor

--- src/screens/messageScreen.js
+++ src/screens/messageScreen.js
@@ -1,45 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import {StatusBar, ToastAndroid} from 'react-native';
 import styled from 'styled-components';
-import GenericButton from '../components/buttons/genericButton';
 import DrawerLoader from '../components/loaders/drawerLoader';
-import MessageItem from '../components/items/messageItem';
+import MessageItem from '../components/items/employeeItem';
 import API from '../api/api';
 
 export default function MessageScreen({route, navigation}) {
   const [loading, setLoading] = useState(true);
-  const [messages, setMessages] = useState([]);
   const [employees, setEmployees] = useState([]);
-
-  let tests = [
-    {
-      id: 1,
-      submit_time: '2021-05-30 09:02:24',
-      update_time: null,
-      text: 'Hello',
-      type: 'INCOMING',
-      status: 'DELIVERED',
-      employee_id: 123456,
-    },
-    {
-      id: 2,
-      submit_time: '2021-05-30 09:04:24',
-      update_time: null,
-      text: 'Hi, How are you',
-      type: 'OUTGOING',
-      status: 'DELIVERED',
-      employee_id: 123456,
-    },
-    {
-      id: 3,
-      submit_time: '2021-05-30 09:04:29',
-      update_time: null,
-      text: 'Whats up?',
-      type: 'OUTGOING',
-      status: 'PENDING',
-      employee_id: 123456,
-    },
-  ];
 
   useEffect(() => {
     // Load from chat endpoint
@@ -49,16 +17,16 @@ export default function MessageScreen({route, navigation}) {
           return a.first_name[0] > b.first_name[0];
         });
         setEmployees(sortedlist);
+        setLoading(false);
       })
       .catch((error) => {
+        setLoading(false);
         ToastAndroid.show(
           error.message,
           ToastAndroid.SHORT,
           ToastAndroid.CENTER,
         );
       });
-    setMessages(tests);
-    setLoading(false);
   }, []);
 
   if (loading) {

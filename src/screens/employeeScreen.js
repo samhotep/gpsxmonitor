eb14cdde5eecd48f9c93b176080bc20230c62a82
Samuel Olwe <sam.olwe@gmail.com>
2021-05-30 20:09:42 +0300

    Load only employees with messages

--- src/screens/employeeScreen.js
+++ src/screens/employeeScreen.js
@@ -1,22 +1,105 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from 'react';
 import {StatusBar, ToastAndroid} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
 import styled from 'styled-components';
 import DrawerLoader from '../components/loaders/drawerLoader';
 import EmployeeItem from '../components/items/employeeItem';
+import LogoTitle from '../components/headers/logoTitle';
 import API from '../api/api';
+import Storage from '../storage/storage';
+import Fuse from 'fuse.js';
 
 export default function EmployeeScreen({route, navigation}) {
   const [loading, setLoading] = useState(true);
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchString, setSearchString] = useState('');
+  const [allEmployees, setAllEmployees] = useState([]);
   const [employees, setEmployees] = useState([]);
+  const [chats, setChats] = useState([]);
+  const isFocused = useIsFocused();
 
-  useEffect(() => {
-    // Load from chat endpoint
-    API.getEmployees()
+  const options = {
+    includeScore: true,
+    threshold: 0.4,
+    keys: ['first_name'],
+  };
+
+  const fuse = new Fuse(allEmployees, options);
+
+  let test = [
+    {
+      id: 1,
+      submit_time: '2021-05-30 09:02:24',
+      update_time: null,
+      text: 'Hello',
+      type: 'INCOMING',
+      status: 'DELIVERED',
+      employee_id: 3484,
+    },
+    {
+      id: 2,
+      submit_time: '2021-05-30 09:04:24',
+      update_time: null,
+      text: 'Hi, How are you',
+      type: 'OUTGOING',
+      status: 'DELIVERED',
+      employee_id: 3484,
+    },
+    {
+      id: 3,
+      submit_time: '2021-05-30 09:04:29',
+      update_time: null,
+      text: 'Whats up?',
+      type: 'OUTGOING',
+      status: 'PENDING',
+      employee_id: 3484,
+    },
+  ];
+
+  const filterBySearch = () => {
+    let searchResults = [];
+    const result = fuse.search(searchString);
+    result.map((res, i) => {
+      searchResults.push(res.item);
+    });
+    if (searchString === '') {
+      setEmployees(allEmployees);
+    } else {
+      setEmployees(searchResults);
+    }
+  };
+
+  const loadObjects = () => {
+    let chats = [];
+    Storage.getCurrentTracker()
+      .then((res) => {
+        let tracker = JSON.parse(res);
+        return API.getChats(tracker.id);
+      })
+      .then((chatlist) => {
+        // chats = chatlist;
+        // setChats(chatlist);
+        // if (chatlist === 400) {
+        //   throw {message: 'No objects available for chat'};
+        // }
+        chats = test;
+        setChats(test);
+        return Storage.getAllEmployees();
+      })
       .then((employeeList) => {
-        let sortedlist = employeeList.sort((a, b) => {
+        let sortedlist = [];
+        JSON.parse(employeeList).map((employee, i) => {
+          if (chats.find((chat) => chat.employee_id === employee.id)) {
+            sortedlist.push(employee);
+          }
+        });
+        sortedlist = sortedlist.sort((a, b) => {
           return a.first_name[0] > b.first_name[0];
         });
         setEmployees(sortedlist);
+        setAllEmployees(sortedlist);
         setLoading(false);
       })
       .catch((error) => {
@@ -27,12 +110,21 @@ export default function EmployeeScreen({route, navigation}) {
           ToastAndroid.CENTER,
         );
       });
-  }, []);
+  };
+
+  useEffect(() => {
+    filterBySearch();
+  }, [searchString]);
+
+  useEffect(() => {
+    loadObjects();
+  }, [isFocused]);
 
   if (loading) {
     return (
       <Container>
         <StatusBar backgroundColor="#007aa6" />
+        <LogoTitle title="Employees" />
         <DrawerLoader />
       </Container>
     );
@@ -40,17 +132,32 @@ export default function EmployeeScreen({route, navigation}) {
 
   return (
     <Container>
-      <ContentContainer>
-        {employees.map((employee, i) => {
-          return (
-            <EmployeeItem
-              employee={employee}
-              onPress={() =>
-                navigation.navigate('MessageScreen', {employee: employee})
-              }
-            />
-          );
-        })}
+      <LogoTitle
+        title="Messages"
+        navigation={navigation}
+        searchString={searchString}
+        setSearchString={setSearchString}
+        showSearch={showSearch}
+        setShowSearch={setShowSearch}
+      />
+      <ContentContainer
+        contentContainerStyle={{
+          alignItems: 'center',
+        }}>
+        {chats === 400 ? (
+          <Text margin={300}>No objects available for chat</Text>
+        ) : (
+          employees.map((employee, i) => {
+            return (
+              <EmployeeItem
+                employee={employee}
+                onPress={() =>
+                  navigation.navigate('MessageScreen', {employee: employee})
+                }
+              />
+            );
+          })
+        )}
       </ContentContainer>
     </Container>
   );
@@ -65,4 +172,14 @@ const Container = styled.Pressable`
 const ContentContainer = styled.ScrollView`
   flex-direction: column;
   width: 100%;
+  background-color: #ffffff;
+`;
+
+const Text = styled.Text`
+  text-align: left;
+  line-height: ${(props) => props.size || 14}px;
+  flex-wrap: wrap;
+  font-size: ${(props) => props.size || 14}px;
+  color: ${(props) => (props.color ? props.color : '#808080')};
+  margin-top: ${(props) => props.margin || 14}px;
 `;

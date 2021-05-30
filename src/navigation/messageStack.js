46839a2f6b61b3f952c191e94cf66ba3c808fa9c
Samuel Olwe <sam.olwe@gmail.com>
2021-05-30 20:13:50 +0300

    Removed unused screen

--- src/navigation/messageStack.js
+++ src/navigation/messageStack.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import TrackerChatScreen from '../screens/trackerChatScreen';
 import EmployeeScreen from '../screens/employeeScreen';
 import MessageScreen from '../screens/messageScreen';
 import styled from 'styled-components';

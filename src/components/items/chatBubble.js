305df2ab99bfccc8955aac01ad91a298aa450379
Samuel Olwe <sam.olwe@gmail.com>
2021-05-31 01:04:47 +0300

    Show message time

--- src/components/items/chatBubble.js
+++ src/components/items/chatBubble.js
@@ -3,35 +3,45 @@ import styled from 'styled-components';
 
 export default function ChatBubble(props) {
   return (
-    <Container outgoing={props.outgoing}>
-      <BubbleContainer outgoing={props.outgoing}>
-        {props.outgoing ? (
-          <>
+    <Container>
+      {props.outgoing ? (
+        <BubbleContainer outgoing={props.outgoing}>
+          <TimeText>{props.time}</TimeText>
+          <MessageContainer outgoing={props.outgoing}>
             <Text outgoing={true}>{props.text}</Text>
             <BubbleCorner source={require('../../assets/sent_triangle.png')} />
-          </>
-        ) : (
-          <>
+          </MessageContainer>
+        </BubbleContainer>
+      ) : (
+        <BubbleContainer outgoing={props.outgoing}>
+          <MessageContainer outgoing={props.outgoing}>
             <BubbleCorner
               source={require('../../assets/received_triangle.png')}
             />
             <Text>{props.text}</Text>
-          </>
-        )}
-      </BubbleContainer>
+          </MessageContainer>
+          <TimeText>{props.time}</TimeText>
+        </BubbleContainer>
+      )}
     </Container>
   );
 }
 
 const Container = styled.View`
   flex-direction: column;
-  align-items: ${(props) => (props.outgoing ? 'flex-end' : 'flex-start')};
   justify-content: center;
   width: 100%;
   padding: 5px;
 `;
 
 const BubbleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const MessageContainer = styled.View`
   flex-direction: row;
   align-items: ${(props) => (props.outgoing ? 'flex-end' : 'flex-start')};
   justify-content: center;
@@ -41,6 +51,7 @@ const BubbleCorner = styled.Image`
   height: 12px;
   width: 12px;
 `;
+
 const Text = styled.Text`
   padding: 10px 10px 15px 10px;
   text-align: left;
@@ -54,4 +65,9 @@ const Text = styled.Text`
   font-size: ${(props) => props.size || 14}px;
   color: ${(props) => (props.outgoing ? '#000000' : '#ffffff')};
   background-color: ${(props) => (props.outgoing ? '#a8caec' : '#66bb6a')};
+`;
+
+const TimeText = styled.Text`
+  font-size: 12px;
+  color: #585858;
 `;

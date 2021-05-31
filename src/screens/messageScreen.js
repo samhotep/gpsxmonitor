f6196a2446e902428292c9c57fa46ce1a110f566
Samuel Olwe <sam.olwe@gmail.com>
2021-05-31 04:54:37 +0300

    Created send button

--- src/screens/messageScreen.js
+++ src/screens/messageScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState, useRef} from 'react';
 import {StatusBar, ToastAndroid} from 'react-native';
@@ -5,6 +6,7 @@ import styled from 'styled-components';
 import DrawerLoader from '../components/loaders/drawerLoader';
 import GenericButton from '../components/buttons/genericButton';
 import LogoTitle from '../components/headers/logoTitle';
+import ChatBubble from '../components/items/chatBubble';
 import API from '../api/api';
 import Storage from '../storage/storage';
 
@@ -12,13 +14,13 @@ export default function MessageScreen({route, navigation}) {
   const [loading, setLoading] = useState(true);
   const [messages, setMessages] = useState([]);
   const [label, setLabel] = useState('');
+  const [sendString, setSendString] = useState('');
   const viewRef = useRef();
   let {employee} = route.params;
 
   useEffect(() => {
     let newLabel = '';
     employee.first_name.split(' ').map((_, i) => {
-      console.log(_);
       if (i !== 0) {
         newLabel += ' ';
       }
@@ -27,7 +29,13 @@ export default function MessageScreen({route, navigation}) {
     setLabel(newLabel);
     Storage.getTrackerMessages()
       .then((res) => {
-        setMessages(JSON.parse(res));
+        let employeeMessages = [];
+        JSON.parse(res).map((_, i) => {
+          if (_.employee_id === employee.id) {
+            employeeMessages.push(_);
+          }
+        });
+        setMessages(employeeMessages);
         setLoading(false);
       })
       .catch((error) => {
@@ -61,13 +69,29 @@ export default function MessageScreen({route, navigation}) {
         onContentSizeChange={() =>
           viewRef.current.scrollToEnd({animated: true})
         }>
-        <GenericButton width={100} />
+        {messages.map((message, i) => {
+          return (
+            <ChatBubble
+              text={message.text}
+              outgoing={message.type === 'OUTGOING'}
+              time={message.submit_time}
+            />
+          );
+        })}
       </ContentContainer>
+      <SendContainer>
+        <SendInput
+          returnKeyType="send"
+          multiline={true}
+          onChangeText={(text) => setSendString(text)}
+        />
+        <GenericButton width={100} title="SEND" />
+      </SendContainer>
     </Container>
   );
 }
 
-const Container = styled.Pressable`
+const Container = styled.View`
   flex: 1;
   flex-direction: column;
   height: 100%;
@@ -75,5 +99,23 @@ const Container = styled.Pressable`
 
 const ContentContainer = styled.ScrollView`
   flex-direction: column;
+  padding: 5px;
   width: 100%;
+`;
+
+const SendContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const SendInput = styled.TextInput`
+  flex-grow: 1;
+  font-size: 16px;
+  margin: 5px;
+  padding: 2px;
+  border-bottom-width: 1px;
+  border-bottom-color: #808080;
+  width: 80px;
 `;
